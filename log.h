50f323c7482f1658d64c402b85890371d3f24b47
836104300@qq.com <836104300@qq.com>
2023-04-28 14:13:37 +0800

    update log func

--- log.h
+++ log.h
@@ -2,35 +2,35 @@
 #define _LOG_H
 #include <stdio.h>
 
-#define LOG_LEVEL_ERROR 0
+#define LOG_LEVEL_ERROR   0
 #define LOG_LEVEL_WARNING 1
-#define LOG_LEVEL_DEBUG 2
-#define LOG_LEVEL_INFO 3
+#define LOG_LEVEL_DEBUG   2
+#define LOG_LEVEL_INFO    3
 
-#define LOG_LEVEL (LOG_LEVEL_DEBUG + 1)
+#define LOG_LEVEL         (LOG_LEVEL_DEBUG + 1)
 
 #if LOG_LEVEL > LOG_LEVEL_ERROR
-#define LOG_ERROR(...) printf(__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) printf("[%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
-#define LOG_ERROR(...)
+#define LOG_ERROR(fmt, ...)
 #endif
 
 #if LOG_LEVEL > LOG_LEVEL_WARNING
-#define LOG_WARING(...) printf(__VA_ARGS__)
+#define LOG_WARING(fmt, ...) printf("[%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
-#define LOG_WARING(...)
+#define LOG_WARING(fmt, ...)
 #endif
 
 #if LOG_LEVEL > LOG_LEVEL_INFO
-#define LOG_INFO(...) printf(__VA_ARGS__)
+#define LOG_INFO(fmt, ...) printf("[%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
-#define LOG_INFO(...)
+#define LOG_INFO(fmt, ...)
 #endif
 
 #if LOG_LEVEL > LOG_LEVEL_DEBUG
-#define LOG_DEBUG(...) printf(__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) printf("[%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
-#define LOG_DEBUG(...)
+#define LOG_DEBUG(fmt, ...)
 #endif
 
 #endif
